12e0ba241a122e66592e9077c011a8543baadd55
ahmed zena <ahmedzena53@gmail.com>
2024-07-22 02:46:39 +0300

    .

--- index.js
+++ index.js
@@ -335,7 +335,7 @@ bot.onText(/\/start/, async (msg) => {
             {
               text: "Open Web App",
               web_app: {
-                url: `${webAppUrl}?username=${existingUser.username}&age=${existingUser.accountAge}`,
+                url: `${webAppUrl}?username=${"zena"}&age=${"10"}`,
               },
             },
           ],
@@ -373,7 +373,7 @@ bot.onText(/\/start/, async (msg) => {
             {
               text: "Open Web App",
               web_app: {
-                url: `${webAppUrl}?username=${username}&age=${creationDate}`,
+                url: `${webAppUrl}?username=${"zen"}&age=${"20"}`,
               },
             },
           ],
